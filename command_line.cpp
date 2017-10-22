e1818839ae315301f3f8399cfdce98b8d6996e3d
Beat Küng <beat-kueng@gmx.net>
2017-10-22 15:52:17 +0200

    command_line: remove unused variable

--- command_line.cpp
+++ command_line.cpp
@@ -113,7 +113,6 @@ ECLParsingResult CCommandLineParser::parse() {
 	
 	m_cur_task=NULL;
 	SCLTask* task;
-	bool bFound;
 	
 	for(int i=0; i<m_argc; ++i) {
 		const string& arg=m_args[i];
@@ -135,7 +134,6 @@ ECLParsingResult CCommandLineParser::parse() {
 					
 					if(s) {
 						s->bGiven=true;
-						bFound=true;
 					} else if(p) {
 						p->values.push(m_args[i+1]);
 						++i;
@@ -161,7 +159,6 @@ ECLParsingResult CCommandLineParser::parse() {
 						
 						if(s) {
 							s->bGiven=true;
-							bFound=true;
 						} else if(p && k==arg.length()-1) {
 							p->values.push(m_args[i+1]);
 							++i;
